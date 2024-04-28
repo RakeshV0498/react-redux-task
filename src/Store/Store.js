9a2e266d706773f2a757f2f064f732c9fff40682
RakeshV0498 <rockr4674@gmail.com>
2024-04-28 13:34:29 +0530

    included cart reducer logic

--- src/Store/Store.js
+++ src/Store/Store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import productReducer from "../Reducers/ProductReducer";
+import cartReducer from "../Reducers/CartReducer";
 
 const Store = configureStore({
   reducer: {
     productReducer,
+    cartReducer,
   },
   devTools: true,
 });
